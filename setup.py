cc39291d911d6f8d6ff339136030b9080c957783
Aaron Schroeder <aaschro@gmail.com>
2021-01-21 13:04:57 -0700

    Tinkering w/ setup version, I SWEAR this will work

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from version import version
-#from distance import __version__
+
+
+def read(rel_path):
+  """Read a file so python does not have to import it.
+  
+  Inspired by (taken from) pip's `setup.py`.
+  """
+  here = os.path.abspath(os.path.dirname(__file__))
+  # intentionally *not* adding an encoding option to open, See:
+  #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+  with open(os.path.join(here, rel_path), 'r') as fp:
+    return fp.read()
+
+
+def get_version(rel_path):
+  """Manually read through a file to retrieve its `__version__`.
+  
+  Inspired by (taken from) pip's `setup.py`.
+  """
+  for line in read(rel_path).splitlines():
+    if line.startswith('__version__'):
+      # __version__ = '0.0.1'
+      delim = "'" if "'" in line else '"'
+      return line.split(delim)[1]
+  raise RuntimeError('Unable to find version string.')
 
 
 with open('README.rst') as f:
@@ -11,12 +34,13 @@ with open('README.rst') as f:
 with open('LICENSE') as f:
   license = f.read()
 
+# Could this be outsourced somehow? find_packages maybe?
+pkg_name = 'distance'
+
 setup(
-  name='distance',
-  version=version,
-  #version=__version__,
-  #version='0.0.1',
-  description='Sample package for Python-Guide.org',
+  name=pkg_name,
+  version=version=get_version('%s/__init__.py' % pkg_name),
+  description='description placeholder',
   long_description=readme,
   author='Aaron Schroeder',
   #author_email='me@kennethreitz.com',
