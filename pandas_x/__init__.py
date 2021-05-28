bbc379d196c20626fb273554427e788657a282dd
Aaron Schroeder <aaschro@gmail.com>
2021-05-28 09:08:58 -0600

    Cleanup before releasing v0.0.2

--- pandas_x/__init__.py
+++ pandas_x/__init__.py
@@ -2,5 +2,5 @@ from .accessor import PositionAccessor
 # from .algorithms import *
 # from .scalar import flat_earth, great_circle, great_circle_geopy
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = ['PositionAccessor']
